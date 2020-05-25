63ab0642d0d451c46b624e3205a2894d240580e5
Lucas Abbade <lucasabbade@hotmail.com>
2020-05-25 01:08:21 -0300

    fastapi

---
+++ fastapi/README.md
@@ -0,0 +1,5 @@
+# FastAPI
+
+## Deployment
+
+Deployment was done mostly accordingly to [FastAPI's website](https://fastapi.tiangolo.com/deployment/), with one difference: the standard Python Docker image was used, instead of the modified uvicorn one. This was done to keep uniformity with the other frameworks.
--- tests/test_api.py
+++ tests/test_api.py
@@ -8,4 +8,4 @@ class TestApi(TestCase):
     def test_hello_world(self):
         r = requests.get('http://localhost/')
         self.assertEqual(r.status_code, 200, msg='root didn\'t return 200')
-        self.assertEqual(r.text, 'Hello World', msg='server response is wrong')
+        self.assertIn('Hello World', r.text, msg='server response is wrong')
