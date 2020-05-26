f551e1708602de13fcdeeed2c67f20cd2c87ab8b
Lucas Abbade <lucasabbade@hotmail.com>
2020-05-26 20:27:42 -0300

    refactoring

--- spring/src/main/java/br/inatel/spring_test/SpringTestApplication.java
+++ spring/src/main/java/br/inatel/spring_test/SpringTestApplication.java
@@ -2,7 +2,6 @@ package br.inatel.spring_test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
