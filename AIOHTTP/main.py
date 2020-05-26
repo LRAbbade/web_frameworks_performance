2b051e4b4d90b0551bbc481ec8e9400d2f514ca8
Lucas Abbade <lucasabbade@hotmail.com>
2020-05-26 20:10:24 -0300

    AIOHTTP done

---
+++ AIOHTTP/main.py
@@ -0,0 +1,12 @@
+from aiohttp import web
+
+
+async def handle(request):
+    return web.Response(text="Hello World")
+
+
+app = web.Application()
+app.add_routes([web.get('/', handle)])
+
+if __name__ == '__main__':
+    web.run_app(app)
